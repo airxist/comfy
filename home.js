e0c92f5bb50522dec945a07cf15f7b907cd386ac
Da-Hubb <josemariaofurum@gmail.com>
2023-03-21 21:27:16 +0100

    made the about page of the project

--- home.js
+++ home.js
@@ -2,17 +2,43 @@
 const openmenu = document.getElementById("menupoper");
 const closemenu = document.querySelector("[data-id = 'closemenu']");
 const compass = document.querySelector(".compass");
+const products = [... document.querySelectorAll(".prod")];
 
-let currentpage;
 
 // ****** CONDITIONS OR OPTIONS
+let currentpage;
 let pausepage = false;
-// const query = matchMedia("(max-width: 768px)");
+
+// ********** EVENT LISTENER
 openmenu.addEventListener("click", openmenubar);
 closemenu.addEventListener("click", closemenubar);
+products.forEach(singleproduct => {
+    singleproduct.addEventListener("click", widen);
+})
 
+
+// listener to stop the scroll function when my menubar is on
+window.addEventListener("scroll", () => {
+    if(pausepage) {
+        window.scrollTo(0, currentpage)
+    }
+})
 
 // ************ FUNCTIONS
+// function widen
+function widen(e) {
+    elem = e.currentTarget;
+    elemchild = elem.querySelector(".prod_details");
+    products.forEach(single => {
+        let singchild = single.querySelector(".prod_details");
+        if(elem != single) {
+            single.classList.remove("showhidden");
+            singchild.classList.remove("show_prod_detail")
+        }
+    })
+    elem.classList.toggle("showhidden")
+    elemchild.classList.toggle("show_prod_detail");
+}
 // function close menu bar
 function closemenubar() {
     compass.classList.remove("show_compass");
@@ -23,9 +49,3 @@ function openmenubar() {
     currentpage = window.pageYOffset;
     pausepage = true;
 }
-
-window.addEventListener("scroll", () => {
-    if(pausepage) {
-        window.scrollTo(0, currentpage)
-    }
-})
